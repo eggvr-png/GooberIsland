7a2b5eb5baac36146158ed8a0a15e4de717a42ca
MaxNiftyNine <83783498+MaxNiftyNine@users.noreply.github.com>
2024-11-29 13:23:42 -0500

    Skip Intro

--- Assets/Scripts/Menu/IntroTimer.cs
+++ Assets/Scripts/Menu/IntroTimer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IntroTimer : MonoBehaviour
@@ -10,16 +9,42 @@ public class IntroTimer : MonoBehaviour
 
     public AudioLowPassFilter filter;
     public AudioReverbFilter filter2;
+    public AudioSource introMusic;
 
-    private IEnumerator Intro(){
-        yield return new WaitForSeconds(introTime);
-        intro.SetActive(false);
-        menu.SetActive(true);
-        Destroy(filter);
-        Destroy(filter2);
+    private bool introSkipped = false;
+
+    private IEnumerator Intro()
+    {
+        float timer = 0f;
+
+        while (timer < introTime && !introSkipped)
+        {
+            timer += Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+            {
+                introSkipped = true;
+            }
+
+            yield return null;
+        }
+
+        if (introSkipped || timer >= introTime)
+        {
+            intro.SetActive(false);
+            menu.SetActive(true);
+            Destroy(filter);
+            Destroy(filter2);
+
+            if (introMusic != null)
+            {
+                introMusic.time = introTime; 
+            }
+        }
     }
 
-    public void startIntro() {
+    public void startIntro() 
+    {
         StartCoroutine(Intro());
     }
 }
